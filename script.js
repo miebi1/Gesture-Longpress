e053560816dd856f46ebbe2e49f4c6e57b8aba40
APPAH OYINMIEBI JOSEPH <109265761+miebi1@users.noreply.github.com>
2024-03-02 02:32:34 +0100

    implementing hammer js on long press gesture

---
+++ script.js
@@ -0,0 +1,26 @@
+// Initialize Hammer on the body
+var hammerLongPress = new Hammer(document.getElementById('gestureBox'));
+
+// Display alert for long press gesture
+alert("Long press to increase the box size.");
+
+// Unique gesture interaction for long press
+hammerLongPress.on("press pressup", function (event) {
+  if (event.type === 'press') {
+    increaseSize();
+  } else {
+    restoreSize();
+  }
+});
+
+function increaseSize() {
+  const gestureBox = document.getElementById('gestureBox');
+  gestureBox.style.width = '18rem';
+  gestureBox.style.height = '18rem';
+}
+
+function restoreSize() {
+  const gestureBox = document.getElementById('gestureBox');
+  gestureBox.style.width = '15rem';
+  gestureBox.style.height = '15rem';
+}
